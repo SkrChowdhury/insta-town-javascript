8c182d5faddf3faf40191ac85302386dea9137c3
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-09-09 13:36:17 +0600

    liked and reported items error fixed

--- js/app.js
+++ js/app.js
@@ -129,9 +129,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments[0].user}
-                      </a>
-                      ${post.comments[0].text}
+                          ${post.comments[0]?.user}
+                      </a>S
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
@@ -153,6 +153,8 @@ const showPosts = (posts) => {
 
 const displayLikedPosts = () => {
   const likedPosts = getLikedPosts();
+  //cleared previously added liked posts
+  document.getElementById('liked').textContent = '';
   likedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById('liked').appendChild(div);
@@ -161,7 +163,8 @@ const displayLikedPosts = () => {
 
 const displayReportedPosts = () => {
   const reportedPosts = getReportedPosts();
-  posts.forEach((post) => {
+  document.getElementById('reported').textContent = '';
+  reportedPosts.forEach((post) => {
     const div = createPost(post);
     document.getElementById('reported').appendChild(div);
   });
